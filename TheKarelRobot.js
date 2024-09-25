84357674628125e6ada561cab7330ddd6b35e45a
Tanvi <145559687+TanviS3000@users.noreply.github.com>
2024-09-25 17:35:52 +0530

    Create TheKarelRobot.js

---
+++ TheKarelRobot.js
@@ -0,0 +1,16 @@
+function main(){
+   moveDiagonally();
+   moveDiagonally();
+   moveDiagonally();
+   moveDiagonally();
+   moveDiagonally();
+}
+function moveDiagonally(){
+   putBeeper();
+   move();
+   turnLeft();
+   move();
+   turnLeft();
+   turnLeft();
+   turnLeft();
+}
